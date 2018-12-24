3c069b291eb162fcf2e174fde22511cc7efffd5e
anstif <stifurak@gmail.com>
2018-12-24 14:21:01 +0200

    profile pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,13 @@
 $(".header__mobile-menu").on("click", function () {
   $(this).toggleClass("open")
 })
+
+var profileClasses = ["profile__progress--is-iron", "profile__progress--is-bronze", "profile__progress--is-silver", "profile__progress--is-gold"]
+
+var i = 0;
+
+setInterval(function () {
+  $(".profile__progress").removeClass(profileClasses[i])
+  $(".profile__progress").addClass(profileClasses[i + 1])
+  i === 3 ? i = -1 : i++;
+}, 1000)
